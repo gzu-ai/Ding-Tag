e130825f10da371041f2ffbedf3d3a6c70664b6f
KunCheng-He <byack-hkc@foxmail.com>
2023-01-08 22:41:41 +0800

    add MediaPlayer

--- src/mainwindow/mainwindow.py
+++ src/mainwindow/mainwindow.py
@@ -1,4 +1,5 @@
 from PySide6.QtWidgets import QMainWindow
+from PySide6.QtMultimedia import QMediaPlayer, QAudioOutput
 
 from .mainwindow_ui import Ui_MainWindow
 
@@ -11,3 +12,9 @@ class MainWindow(Ui_MainWindow):
         self.main = main
         self.main_win = QMainWindow()
         self.setupUi(self.main_win)
+
+        # 媒体播放器
+        self.player = QMediaPlayer()
+        self.audioOutput = QAudioOutput()
+        self.player.setAudioOutput(self.audioOutput)
+        self.audioOutput.setVolume(1)  # 设置音量，范围为0~1
