66a4e952289860c28682bcffa57940a9eb410cbb
KunCheng-He <byack-hkc@foxmail.com>
2023-01-09 21:13:41 +0800

    存储标注结果

--- src/mainwindow/mainwindow.py
+++ src/mainwindow/mainwindow.py
@@ -33,11 +33,13 @@ class MainWindow(Ui_MainWindow):
         self.replay.clicked.connect(self.replay_button)
         self.pleasure_info.valueChanged.connect(self.pleasure_change)
         self.action_info.valueChanged.connect(self.action_change)
+        self.submit.clicked.connect(self.submit_event)
     
     def setting_player(self):
         """ 设置播放器 """
         if self.main.ding["id"] == self.main.ding["end"]:  # 标注完成
-            QMessageBox.information(self.main_win, "提示", "该数据已标注完成，请退出程序")
+            QMessageBox.information(self.main_win, "提示", "该数据已标注完成，将自动退出程序")
+            self.main_win.close()
         else:
             self.path_info.setText(
                 self.main.ding["filelist"][self.main.ding["id"]].split(os.sep)[1]
@@ -97,3 +99,73 @@ class MainWindow(Ui_MainWindow):
     def action_change(self, value):
         """ 激活维滑动的值 """
         self.action_show.setText("{:.1f}".format(value / 10))
+    
+    def info_table(self):
+        """ 获取所有标注信息 """
+        info_table = {
+            "name": self.name_info.text(),
+            "flag1": self.flag1.isChecked(), "flag2": self.flag2.isChecked(),
+            "flag3": self.flag3.isChecked(), "flag4": self.flag4.isChecked(),
+            "role": "", "emotion": "",
+            "text": self.text_info.toPlainText(),
+            "pleasure": self.pleasure_show.text(), "action": self.action_show.text()
+        }
+        # 单独处理角色和离散标签
+        value = self.emotion_info.currentText()
+        if not value == "":
+            info_table["role"], info_table["emotion"] = value.split("-")
+        return info_table
+    
+    def save_result(self, info_table):
+        """ 存储标注结果 """
+        with open(os.path.join(self.main.info["savepath"], "result.csv"), "a") as f:
+            f.write(
+                "{},{},{},{},{},{},{},{},{},{},{}\n".format(
+                    self.path_info.text(),
+                    info_table["name"], info_table["flag1"], info_table["flag2"],
+                    info_table["flag3"], info_table["flag4"], info_table["role"],
+                    info_table["text"], info_table["emotion"],
+                    info_table["pleasure"], info_table["action"]
+                )
+            )
+    
+    def recovery_ding(self):
+        """ 将标注项全部复原 """
+        self.flag1.setChecked(False)
+        self.flag2.setChecked(False)
+        self.flag3.setChecked(False)
+        self.flag4.setChecked(False)
+        self.text_info.clear()
+        self.emotion_info.setCurrentIndex(-1)
+        self.pleasure_info.setValue(0)
+        self.action_info.setValue(0)
+    
+    def submit_event(self):
+        """ 提交标注信息 """
+        info_table = self.info_table()
+        # 检查音频有无异常情况下
+        flag_normal = True
+        for flag in ["flag1", "flag2", "flag3", "flag4"]:
+            if info_table[flag]:
+                flag_normal = False
+                break
+        # 音频无异常情况，则所有字段不能为空
+        normal = True
+        if flag_normal:
+            for check in ["role", "emotion", "text"]:
+                if info_table[check] == "":
+                    QMessageBox.warning(self.main_win, "警告", f"音频无异常，音频文本与情感分类不能为空")
+                    normal = False
+                    break
+        # 通过检查，请用户确认
+        if normal:
+            box = QMessageBox.question(self.main_win, "用户确定", "确定该样本标注无误吗？")
+            if box == QMessageBox.Yes:
+                # 标注无误，存储结果
+                self.save_result(info_table)
+                # 修改样本索引
+                self.main.ding["id"] += 1
+                # 重新设置播放器
+                self.setting_player()
+                # 重新设置标注项
+                self.recovery_ding()
